0a751456fd06e83f53d3e51e6d777a012cd25764
Dmitriy Medvedko <md-rs4@mail.ru>
2024-02-22 17:24:02 +0400

    +rub

--- app/src/main/java/com/example/kotlinreg/ItemCardActivity.kt
+++ app/src/main/java/com/example/kotlinreg/ItemCardActivity.kt
@@ -2,18 +2,44 @@ package com.example.kotlinreg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
-class ItemCardActivity : AppCompatActivity() {
+class ItemCardActivity() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ItemCardActivity> {
+        override fun createFromParcel(parcel: Parcel): ItemCardActivity {
+            return ItemCardActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ItemCardActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_card)
 
+
         val title: TextView = findViewById(R.id.item_list_titleOne)
         val text: TextView = findViewById(R.id.item_list_text)
-        val cimage: ImageView = findViewById(R.id.item_card_image)
-
+        val btn: Button = findViewById(R.id.item_list_button)
         title.text = intent.getStringExtra("itemTitle")
         text.text = intent.getStringExtra("itemText")
     }
